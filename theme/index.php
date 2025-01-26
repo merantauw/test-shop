f92ec87edbb5fa1efcaa65c28f15405513ffa30c
Merantauw <zholu.vlad2010@yandex.ru>
2025-01-26 19:22:12 +0500

    Revert "удалил базовые файлы, добавил с нуля и подключил файлы для изучения компонента"
    This reverts commit 30b7be893edad279f7f44089caa36e195c672334.

--- theme/index.php
+++ theme/index.php
@@ -1,10 +1,9 @@
-<?php get_header(); ?>
-
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-
-<?php endwhile; else: ?>
-    <p>Записей нет</p>
-<?php endif; ?>
-
-<?php get_footer(); ?>
+<?php
+/**
+ * This file has been left empty on purpose.
+ *
+ * @link https://core.trac.wordpress.org/ticket/54272
+ *
+ * @package merantauw-theme
+ * @since 1.0.0
+ */
