5ecbaae1b862500cccb37472286e43033685af35
Merantauw <zholu.vlad2010@yandex.ru>
2025-01-26 19:33:29 +0500

    заменил файлы ручками, так как не было ошибок

--- theme/functions.php
+++ theme/functions.php
@@ -1,26 +1,5 @@
 <?php
-/**
- * Functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package merantauw-theme
- * @since 1.0.0
- */
-
-/**
- * Enqueue the CSS files.
- *
- * @since 1.0.0
- *
- * @return void
- */
-function merantauw_theme_styles() {
-	wp_enqueue_style(
-		'merantauw-theme-style',
-		get_stylesheet_uri(),
-		[],
-		wp_get_theme()->get( 'Version' )
-	);
-}
-add_action( 'wp_enqueue_scripts', 'merantauw_theme_styles' );
+//https://developer.woocommerce.com/docs/classic-theme-development-handbook/
+add_action('after_setup_theme', function () {
+    add_theme_support('woocommerce');
+});
